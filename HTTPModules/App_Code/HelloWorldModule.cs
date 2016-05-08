d179db576f7eab250295c0613fc5009e66219ccb
giuseppemag <giuseppemag@gmail.com>
2016-05-08 13:39:13 +0200

    Added basic experiments on a custom AJAX framework.

---
+++ HTTPModules/App_Code/HelloWorldModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web;
+public class HelloWorldModule : IHttpModule
+{
+  public HelloWorldModule()
+  {
+  }
+
+  public String ModuleName
+  {
+    get { return "HelloWorldModule"; }
+  }
+
+  // In the Init function, register for HttpApplication 
+  // events by adding your handlers.
+  public void Init(HttpApplication application)
+  {
+    application.BeginRequest +=
+        (new EventHandler(this.Application_BeginRequest));
+    application.EndRequest +=
+        (new EventHandler(this.Application_EndRequest));
+  }
+
+  private void Application_BeginRequest(Object source,
+       EventArgs e)
+  {
+    // Create HttpApplication and HttpContext objects to access
+    // request and response properties.
+    HttpApplication application = (HttpApplication)source;
+    HttpContext context = application.Context;
+    string filePath = context.Request.FilePath;
+    string fileExtension =
+        VirtualPathUtility.GetExtension(filePath);
+    if (fileExtension.Equals(".html"))
+    {
+      //context.Response.Write(@"<div><font color=red>Login form goes here</font></div>");
+    }
+  }
+
+  private void Application_EndRequest(Object source, EventArgs e)
+  {
+    HttpApplication application = (HttpApplication)source;
+    HttpContext context = application.Context;
+    string filePath = context.Request.FilePath;
+    string fileExtension =
+        VirtualPathUtility.GetExtension(filePath);
+    if (fileExtension.Equals(".html"))
+    {
+      //context.Response.Write(
+      //@"<footer>
+      //  <p>Made by: Giuseppe Maggiore</p>
+      //  <p>Contact information: <a href=""mailto: someone@example.com"">
+      //  someone@example.com </a>.</p>
+      //</footer>");
+    }
+  }
+
+  public void Dispose() { }
+}
